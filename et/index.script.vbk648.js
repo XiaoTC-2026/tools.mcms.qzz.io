6f582e4e5189aa7dd428ddee399b38504d819bda
XiaoTC-2026 <xiaotc_2024@163.com>
2026-03-15 15:45:18 +0800

    Update index.script.vbk648.js

--- et/index.script.vbk648.js
+++ et/index.script.vbk648.js
@@ -1,20 +1,2 @@
-function showToast() {
-    const toast = document.getElementById('copy-toast');
-    if (!toast) return;
-    toast.classList.remove('show');
-    void toast.offsetWidth; 
-    toast.classList.add('show');
-    setTimeout(() => {
-        toast.classList.remove('show');
-    }, 5000);
-}
-async function copyCmd(){
-    const txt = document.getElementById('tpCmd').textContent;
-    await navigator.clipboard.writeText(txt);
-    showToast();
-}
-async function copyCoord(id){
-    const txt = document.getElementById(id).textContent;
-    await navigator.clipboard.writeText(txt);
-    showToast();
-}
+/*history*/
+/*也许可以期待一下*/

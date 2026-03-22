06b5e30386100d3400e172cc2328111f7f989195
XiaoTC-2026 <xiaotc_2024@163.com>
2026-03-22 15:45:52 +0800

    Update index.script.d15gf3.js

--- et/index.script.d15gf3.js
+++ et/index.script.d15gf3.js
@@ -1,3 +1,1 @@
-if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
-    alert("检测到手机访问，本页面为电脑版布局，请在电脑浏览器中查看以获得最佳体验。");
-}
+if(/Mobi|Android|iPhone/i.test(navigator.userAgent)){alert("检测到手机访问，本页面为电脑版布局，请在电脑浏览器中查看以获得最佳体验。");}
